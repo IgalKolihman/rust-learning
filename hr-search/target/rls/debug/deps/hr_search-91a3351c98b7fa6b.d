3aa93a91c6e538cb82eaa92c8e63498522c9bf30
IgalKolihman <igal.kolihman@gmail.com>
2022-09-24 12:07:13 +0300

    hr-search data and skeleton

---
+++ hr-search/target/rls/debug/deps/hr_search-91a3351c98b7fa6b.d
@@ -0,0 +1,5 @@
+d:\Projects\Rust\rust-learning\hr-search\target\rls\debug\deps\hr_search-91a3351c98b7fa6b.rmeta: src\main.rs
+
+d:\Projects\Rust\rust-learning\hr-search\target\rls\debug\deps\hr_search-91a3351c98b7fa6b.d: src\main.rs
+
+src\main.rs:
---
+++ hr-search/target/rls/debug/deps/hr_search-e825e1f98cfd1dea.d
@@ -0,0 +1,5 @@
+d:\Projects\Rust\rust-learning\hr-search\target\rls\debug\deps\hr_search-e825e1f98cfd1dea.rmeta: src\main.rs
+
+d:\Projects\Rust\rust-learning\hr-search\target\rls\debug\deps\hr_search-e825e1f98cfd1dea.d: src\main.rs
+
+src\main.rs:
